8b84c799a8460193612ced386735b70d2f6ee1ed
noakol <noa.kolodro@amdocs.com>
2017-06-24 23:23:00 +0300

    externals dependencies v1.0.2

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import _ from 'lodash';
 
 const el = document.createElement('div');
 document.body.appendChild(el);
