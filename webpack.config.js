be19c225f04ac6f0df67ac8cdf0459567f48b3fd
noakol <noa.kolodro@amdocs.com>
2017-06-25 09:00:59 +0300

    externals dependencies v1.0.4

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const { resolve } = require('path');
+const PATH = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
@@ -11,18 +11,18 @@ module.exports = {
   },
   externals: Object.keys(pkg.dependencies),
   output: {
-    path: resolve(__dirname, 'dist'),
+    path: PATH.resolve(__dirname, 'dist'),
     filename: '[name].js',
     library: 'widgetB',
     libraryTarget: 'umd'
   },
 
-  context: resolve(__dirname, 'src'),
+  context: PATH.resolve(__dirname, 'src'),
 
   module: {
     rules: [{
       test: /.js|.jsx?$/,
-      include: resolve(__dirname, 'src'),
+      include: PATH.resolve(__dirname, 'src'),
       loader: 'babel-loader',
       options: {
         presets: [['env', { modules: false }], 'react']
